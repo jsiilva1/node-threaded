35f8a3421bd74ef20505c413b73b418a6057db42
jsiilva1 <jsiilva@outlook.com.br>
2020-01-31 16:58:13 -0300

    Added the good log output integrated

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
+/*
+ * System dependencies
+*/
 import cluster from 'cluster';
-import { bootstrap } from './src/app';
 import os from 'os';
+
+/*
+ * Application dependencies
+*/
+import { bootstrap } from './src/app';
+import log from './src/log';
 
 const CPUs = os.cpus().length;
 
 if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running!`);
+  log.info(`Master ${process.pid} is running!`);
 
   for (let i = 0; i < CPUs; i++) {
     cluster.fork();
   }
 
   cluster.on('exit', (worker) => {
-    console.log(`Worker ${worker.process.pid} is running!`);
+    log.info(`Worker ${worker.process.pid} is running!`);
   });
 } else {
   bootstrap()
     .then((app) => {
       app.listen(5000, () => console.log('App running on port 5000'));
       
-      console.log(`Worker ${process.pid} is running!`);
+      log.info(`Worker ${process.pid} is running!`);
     })
     .catch((err) => {
-      console.log(err);
+      log.error(err);
+      
       process.exit(1);
     });
 }
